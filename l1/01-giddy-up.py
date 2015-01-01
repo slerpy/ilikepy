affb870083f4caa4739874343eea3d85f6de8d26
slerpy <slerpy@riseup.net>
2015-01-01 07:02:33 -0800

    omnomnom

--- l1/01-giddy-up.py
+++ l1/01-giddy-up.py
@@ -25,19 +25,37 @@
 # url=page[start_quote+1:end_quote]
 # print url
 
+# above worked as well, commented out so i dont lose it.
+#
+
 page=('<html xmlns="http://www.w3.org/1999/xhtml"><head><title>Udacity</title></head><body><h1>Udacity</h1><p><b>Udacity</b> is a private institution of<a href="http://www.wikipedia.org/wiki/Higher_education">higher education founded by</a> <a href="http://www.wikipedia.org/wiki/Sebastian_Thrun">Sebastian Thrun</a>, David Stavens, and Mike Sokolsky with the goal to provide university-level education that is "both high quality and low cost".</p>  <p> It is the outgrowth of a free computer science class offered in 2011 through Stanford University. Currently, Udacity is working on its second course on building a search engine. Udacity was announced at the 2012 <a href="http://www.wikipedia.org/wiki/Digital_Life_Design">Digital Life Design</a> conference.</p>     </body></html>')
+
 start_link=page.find("<a href=")
 start_quote=page.find('"',start_link)
 end_quote=page.find('"',start_quote+1)
 url=page[start_quote+1:end_quote]
 print url
+
 start_link=page.find("<a href=",end_quote)
 start_quote=page.find('"',start_link)
 end_quote=page.find('"',start_quote+1)
 url=page[start_quote+1:end_quote]
 print url
+
 start_link=page.find("<a href=",end_quote)
 start_quote=page.find('"',start_link)
 end_quote=page.find('"',start_quote+1)
 url=page[start_quote+1:end_quote]
 print url
+
+#
+# round and round we go. i totally cheaterfaced and looked up the .5 part.
+# all code after lightbulbing on the rounding bits totally did not cheat.
+# p.s. the dopamine shot from lightbulbing on a concept is quite lovely.
+#
+
+x = 5.64159
+y = str(x + .5)
+dec = y.find(".")
+x = y[:dec]
+print x

60588ec42d867f0e55828a38f0c31fb7aac5593e
slerpy <tonyoh@gmail.com>
2015-01-04 21:30:30 -0800

    to be continued...

--- l2/2.01-repetition.py
+++ l2/2.01-repetition.py
@@ -116,4 +116,33 @@ print is_friend("Donald McRonald")
 #
 
 
+###
+# using 'or': i teensy weensy cheated and used 'and' rather than 'or'
+###
 
+# Define a procedure, biggest, that takes three
+# numbers as inputs and returns the largest of
+# those three numbers.
+
+def biggest(a,b,c):
+    if a>b and a>c:
+        return a
+    else:
+        if b>c:
+            return b
+        else:
+            return c
+
+
+
+#print biggest(3, 6, 9)
+#>>> 9
+
+#print biggest(6, 9, 3)
+#>>> 9
+
+#print biggest(9, 3, 6)
+#>>> 9
+
+print biggest(3, 3, 9)
+#>>> 9

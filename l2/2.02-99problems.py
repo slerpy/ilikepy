087366a344552e3803cf14a14fd7fa5ba9c9838b
slerpy <slerpy@riseup.net>
2015-01-15 05:29:15 -0800

    ok gittin this git back on track. double commits spurting all over the place earlier.

---
+++ l2/2.02-99problems.py
@@ -0,0 +1,35 @@
+####
+# a procedure to add one day to a calendar, assuming all months are 30 days.
+# a test run into building a full calendar.
+####
+
+def nextDay(year, month, day):
+    if day == 30:
+        day = 1
+        if month == 12:
+            month = 1
+            year = year + 1
+        else:
+            month = month + 1
+    else:
+        day = day + 1
+
+    return(year, month, day)
+<<<<<<< HEAD
+print nextDay(2011, 12, 1)
+
+###
+# another way to do above, not sure which is going to prove more useful.
+###
+
+def nextDay2(year, month, day):
+    if day < 30:
+        return year, month, day + 1
+    else:
+        if month < 12:
+            return year, month + 1, 1
+        else:
+            return year +1, 1, 1
+print nextDay2(2098, 11, 13)
+=======
+print nextDay(2011, 12, 1)

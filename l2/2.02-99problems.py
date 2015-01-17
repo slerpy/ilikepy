5ab2169818579820bc5b02e6fe9a5c020e0ff042
slerpy <slerpy@riseup.net>
2015-01-17 02:42:08 -0800

    tweaked daysBetweenDates stuff, should work on negatives now. Next step, add leap year and proper days in month.

--- l2/2.02-99problems.py
+++ l2/2.02-99problems.py
@@ -3,7 +3,7 @@
 # a test run into building a full calendar.
 ####
 
-def nextDay(year, month, day):
+def nextDayMeh(year, month, day):
     if day == 30:
         day = 1
         if month == 12:
@@ -15,14 +15,14 @@ def nextDay(year, month, day):
         day = day + 1
 
     return(year, month, day)
-<<<<<<< HEAD
-print nextDay(2011, 12, 1)
+
+print nextDayMeh(2011, 12, 1)
 
 ###
-# a more eloquent way to do above, not sure which is going to prove more useful.
+# a more elegant way to do above, not sure which is going to prove more useful.
 ###
 
-def nextDay2(year, month, day):
+def nextDay(year, month, day):
     if day < 30:
         return year, month, day + 1
     else:
@@ -30,11 +30,12 @@ def nextDay2(year, month, day):
             return year, month + 1, 1
         else:
             return year +1, 1, 1
-print nextDay2(2098, 11, 13)
-=======
+print nextDay(2098, 11, 13)
+
 print nextDay(2011, 12, 1)
 
 
+###
 # a daysBetweenDates procedure that would produce the
 # correct output if there was a correct nextDay procedure.
 #
@@ -42,6 +43,8 @@ print nextDay(2011, 12, 1)
 # our nextDay procedure assumes all months have 30 days
 # (hence a year is 360 days, instead of 365).
 #
+# this will surely not work with months that lead to negs when subtracted.
+###
 
 
 def daysBetweenDates(year1, month1, day1, year2, month2, day2):
@@ -50,15 +53,25 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
        in Gregorian calendar, and the first date is not after
        the second."""
 
-    years = year2 - year1
-    months = month2 - month1
-    days = day2 - day1
-    result = years * 360 + months * 30 + days
-    print result
-    return result
+#    years = year2 - year1
+#    months = month2 - month1
+#   days = day2 - day1
+#    result = years * 360 + months * 30 + days
+#    print result
+#    return result
+
+###abdhik
+# this is a little sexier than the above. works with negs.
+###
+
+    daysN = 0
+    while year1 != year2 or month1 != month2 or day1 != day2:
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        daysN = daysN + 1
+    return daysN
 
 def test():
-    test_cases = [((2012,9,30,2012,10,30),30),
+    test_cases = [((2012,9,30,2012,10,20),20),
                   ((2012,1,1,2013,1,1),360),
                   ((2012,9,1,2012,9,4),3)]
 

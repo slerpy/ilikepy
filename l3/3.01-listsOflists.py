c01c12de1262999098a8da966000b3ef57f6c99c
slerpy <slerpy@riseup.net>
2015-01-24 05:09:14 -0800

    Mutants. Little fetus sized mutations are here.

--- l3/3.01-listsOflists.py
+++ l3/3.01-listsOflists.py
@@ -1,12 +1,43 @@
 ###
-# as we move into chapter 3 of our adventures, we appear to learning how to make
+# as we move into chapter 3 of our adventures, we appear to be learning how to make
 # lists. are these the same as arrays?
+###
+
+
+###
+# procedure to figure out how many days in any chosen month
+# assumes non leap year
 ###
 
 days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-def how_many_days(daysN):
-    daysN = days_in_month[daysN - 1]
-    return daysN
+
+def how_many_days(month):
+    return days_in_month[month - 1]
 print how_many_days(11)
+
+
+###
+# just testing slerping stuff out from lists
+###
+
+countries = [['China','Beijing',1350],
+             ['India','New Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington DC',307]]
+
+# india capital
+print countries[1][1]
+# multiple of romanias pop to china
+print countries[0][2] / countries[2][2]
+
+
+###
+# mutate stooges to get rid of curly and replace with shemp
+###
+
+
+stooges = ['Moe','Larry','Curly']
+stooges[2] = 'Shemp'
+print stooges
 

7e3d1358fb7b9489dac011f490d2fa2041e525b7
slerpy <slerpy@riseup.net>
2015-02-13 05:39:20 -0800

    keepin on with the keepin on of data structuring.

--- l4/4-01-queries.py
+++ l4/4-01-queries.py
@@ -43,3 +43,28 @@ add_to_index(index, 'udacity', 'http://npr.org')
 print index
 #>>> [['udacity', ['http://udacity.com', 'http://npr.org']], 
 #>>> ['computing', ['http://acm.org']]]
+
+# A procedure, lookup,
+# that takes two inputs:
+
+# - an index
+# - keyword
+
+# The procedure should return a list
+# of the urls associated
+# with the keyword. If the keyword
+# is not in the index, the procedure
+# should return an empty list.
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+
+def lookup(index, keyword):
+    for e in index:
+        if e[0] == keyword:
+            return e[1]
+    return []
+
+print lookup(index,'udacity')
+#>>> ['http://udacity.com','http://npr.org']

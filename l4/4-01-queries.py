81834b193c123adafacc57fbacefa29554ff7eb2
slerpy <slerpy@riseup.net>
2015-02-14 00:40:53 -0800

    more on queries, removed slerp.py

--- l4/4-01-queries.py
+++ l4/4-01-queries.py
@@ -11,25 +11,10 @@
 # @version 0.0.1
 ##############################################################################
 
-
-#add_to_index,
-# takes 3 inputs:
-# - an index: [[<keyword>,[<url>,...]],...]
-# - a keyword: String
-# - a url: String
-
-# If the keyword is already
-# in the index, add the url
-# to the list of urls associated
-# with that keyword.
-
-# If the keyword is not in the index,
-# add an entry to the index: [keyword,[url]]
-
 index = []
 
 
-def add_to_index(index, keyword, url):
+def add_to_index(index, keyword, url):  # populate our index, but first make sure keyword is not already in list.
     for e in index:
         if e[0] == keyword:
             e[1].append(url)
@@ -37,34 +22,18 @@ def add_to_index(index, keyword, url):
     index.append([keyword,[url]])
 
 
-add_to_index(index, 'udacity', 'http://udacity.com')
-add_to_index(index, 'computing', 'http://acm.org')
-add_to_index(index, 'udacity', 'http://npr.org')
-print index
-#>>> [['udacity', ['http://udacity.com', 'http://npr.org']], 
-#>>> ['computing', ['http://acm.org']]]
-
-# A procedure, lookup,
-# that takes two inputs:
-
-# - an index
-# - keyword
-
-# The procedure should return a list
-# of the urls associated
-# with the keyword. If the keyword
-# is not in the index, the procedure
-# should return an empty list.
-
-index = [['udacity', ['http://udacity.com', 'http://npr.org']],
-         ['computing', ['http://acm.org']]]
-
-
-def lookup(index, keyword):
+def lookup(index, keyword):     # lookup keywords in our list.
     for e in index:
         if e[0] == keyword:
             return e[1]
     return []
 
-print lookup(index,'udacity')
-#>>> ['http://udacity.com','http://npr.org']
+
+def add_page_to_index(index,url,content):   # .split all words into a list and run them through the add_to_index
+    for words in content.split():           #  procedure to populate our index.
+        add_to_index(index, words, url)
+
+
+
+add_page_to_index(index,'fake.text',"This is a test")
+print index

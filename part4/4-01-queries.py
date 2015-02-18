aab69be8c684c5a157c4f68c88f910b63b5163ea
slerpy <slerpy@riseup.net>
2015-02-18 00:51:06 -0800

    fixing shortcomings of .split

--- part4/4-01-queries.py
+++ part4/4-01-queries.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-# 01-quieries.py
+# 01-queries.py
 # --------------------
 # The next chapter in our adventures in python saga. giddyup!
 #
@@ -37,3 +37,31 @@ def add_page_to_index(index,url,content):   # .split all words into a list and r
 
 add_page_to_index(index,'fake.text',"This is a test")
 print index
+
+
+def split_string(source,splitlist):  # take two inputs, a string to split, and a list of characters as splitpoints.
+    output = []
+    split = True
+    for e in source:
+        if e in splitlist:
+            split = True
+        else:
+            if split:
+                output.append(e)
+                split = False
+            else:
+                output[-1] = output[-1] + e
+    return output
+
+
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print out
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print out
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print out
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']

829849ba284de6e30bfc00f6aa877e2288bacd38
slerpy <slerpy@riseup.net>
2015-02-21 14:13:46 -0800

    added urllib2. procedures seem to function as they should.

--- part5/hashing.py
+++ part5/hashing.py
@@ -10,7 +10,9 @@
 # @date 21 February 2015
 # @version 0.0.1
 ##############################################################################
-
+import urllib2
+def get_page(url):
+    return urllib2.urlopen(url).read()
 
 #print ord('a')
 #print ord('A')
@@ -27,6 +29,20 @@ print hash_string('b', 12) # 2
 print hash_string('a', 13) # 6
 print hash_string('au', 12) # 10
 print hash_string('uberhashed', 12) # 7
+
+def test_hash_function(func, keys, size):
+    results = [0] * size
+    keys_used = []
+    for w in keys:
+        if w not in keys_used:
+            hv = func(w, size)
+            results[hv] += 1
+            keys_used.append(w)
+    return results
+
+words = get_page('http://www.gutenberg.org/cache/epub/1661/pg1661.txt').split()
+counts = test_hash_function(hash_string, words, 100)
+print counts
 
 
     

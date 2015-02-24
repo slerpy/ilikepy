8459122cbc9b4259523462bd6dc45191eee974f9
slerpy <slerpy@riseup.net>
2015-02-24 01:57:09 -0800

    more work on hashtablejive

--- part5/hashing.py
+++ part5/hashing.py
@@ -20,15 +20,15 @@ def get_page(url):
 
 
 def hash_string(keyword, buckets):  # this should give us a modulo that
-    x = 0                           # will distribute evenly amongst a hash table?
+    out = 0                         # will distribute evenly amongst a hash table?
     for e in keyword:
-        x = ord(e) + x
-    return x % buckets
-print hash_string('a', 12) # 1
-print hash_string('b', 12) # 2
-print hash_string('a', 13) # 6
-print hash_string('au', 12) # 10
-print hash_string('uberhashed', 12) # 7
+        out = (out + ord(s)) % buckets
+    return out
+#print hash_string('a', 12) # 1
+#print hash_string('b', 12) # 2
+#print hash_string('a', 13) # 6
+#print hash_string('au', 12) # 10
+#print hash_string('uberhashed', 12) # 7
 
 def test_hash_function(func, keys, size):
     results = [0] * size
@@ -43,22 +43,35 @@ def test_hash_function(func, keys, size):
 # uncommented line just to stop it from pestering gutenberg
 #words = get_page('http://www.gutenberg.org/cache/epub/1661/pg1661.txt').split()  #
 counts = test_hash_function(hash_string, words, 100)
-print counts
+#print counts
 
 
-def make_hashtable(nbuckets):   # create our empty buckets
-    nb = 0                      # number of buckets counter
-    hashtable = []              # empty hashtable to start with
-    while n < nb:               # loop through and do your job.
-        hashtable.append([])
-        nb += 1
-    return ht
+# def make_hashtable(nbuckets):   # create our empty buckets
+#     nb = 0                      # number of buckets counter
+#     hashtable = []              # empty hashtable to start with
+#     while n < nb:               # loop through and do your job.
+#         hashtable.append([])
+#         nb += 1
+#     return ht
 
-def make_hashtable_better(nbuckets):        # better version of above
+def make_hashtable(nbuckets):        # better version of above
     hashtable = []                          # better because: shorter, and
-    for useless_var in range(0,nbuckets):   # removes danger of forgetting to increment counter
+    for useless_var in range(0,nbuckets):   # removes danger of forgetting the increment counter
         table.append([])
     return hashtable
+
+def hashtable_get_bucket(htable,keyword):           # track down bucket location
+    return htable[hash_string(keyword,len(htable))]
+
+
+# def hashtable_add(htable,key,value):              # add key and value to ord slot from hash_string
+#     drop_in = hash_string(key,len(htable))
+#     htable[drop_in].append([key,value])
+#     return htable
+
+
+def hashtable_add(htable, key, value):              # same as above, only done the way the example does it.
+    hashtable_get_bucket(htable, key).append([key, value])
 
 
 

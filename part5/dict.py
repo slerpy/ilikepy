dd0cf936956d00ee6ee349e4338aa06a3334316f
slerpy <slerpy@riseup.net>
2015-03-07 03:51:36 -0800

    updating our lookups/updates to use python hashing

--- part5/dict.py
+++ part5/dict.py
@@ -114,3 +114,27 @@ print involved(courses, 'Dave')
 
 print involved(courses, 'Peter C.')
 #>>> {'apr2012': ['cs262'], 'feb2012': ['cs101']}
+
+
+
+def bucket_find(bucket, key):                               # find bucket in table
+    for entry in bucket:
+        if entry[0] == key:
+            return entry
+    return None
+
+
+def hashtable_update(htable, key, value):                   # update dictionary with new key, and/or new value
+    bucket = hashtable_get_bucket(htable, key)
+    entrry = bucket_find(bucket, key)
+    if entry:
+        entry[1] = value
+    else:
+        bucket.append([key, value])
+
+def hashtable_lookup(htable, key):                          #find key from bucket_find
+    entry = bucket_find(hashtable_get_bucket(htable, key))
+    if entry:
+        return entry[1]
+    else:
+        return None
